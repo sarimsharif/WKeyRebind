fc40bc87624bee3e37848a1c4a6837646e2bb1ee
sarimsharif <sarimsharif.2012@gmail.com>
2025-01-11 18:59:19 +0600

    Version 1.0 code released

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
+import ctypes
 import pyautogui
 import keyboard
 
-# Define the rebind functionality
-def rebind_f6_to_w():
-    while True:
-        # Check if F6 is pressed
-        if keyboard.is_pressed("F6"):
-            # Simulate pressing 'W'
-            pyautogui.press("w")
+# Set the custom window name for Task Manager
+def set_process_name(name):
+    ctypes.windll.kernel32.SetConsoleTitleW(name)
 
-            # Prevent holding the key too long
-            while keyboard.is_pressed("F6"):
-                pass  # Wait for the key to be released
+# Custom names
+set_process_name("KeyboardRebind")
 
+def on_f6_pressed():
+    pyautogui.press("w")
 
-# Run the rebind function
-if __name__ == "__main__":
-    print("Press F6 to send 'W'. Press Ctrl+C to exit.")
-    try:
-        rebind_f6_to_w()
-    except KeyboardInterrupt:
-        print("Rebind program stopped.")
+def on_f12_pressed():
+    pyautogui.press("right")
+
+# Register hotkeys
+keyboard.add_hotkey('F6', on_f6_pressed, args=(), suppress=True)
+keyboard.add_hotkey('F12', on_f12_pressed, args=(), suppress=True)
+
+print("Press F6 to send 'W' and F12 to send 'Left Arrow'. Press Ctrl+C to exit.")
+
+# Keep the program running to listen for key presses
+keyboard.wait()
